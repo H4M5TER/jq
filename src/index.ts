6ed46c3fe6af206290b1f8646f51fa47c8dec3cb
H4M5TER <h4m5ter@outlook.com>
2024-09-19 07:09:06 +0800

    v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Context, Schema } from 'koishi'
+import { compile, formats } from './jq'
 
 export const name = 'jq'
 
@@ -7,5 +8,12 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('jq <json> <expr:text>')
+    .action(({ session }, json: string, expr: string) => {
+      const object = (() => { try { return JSON.parse(json) } catch (e) {} })()
+      if (!object) return 'Invalid JSON.'
+      const filter = compile(expr)
+      const result: Generator = filter(object)
+      return [...result].map(formats.text).join('\n')
+    })
 }
